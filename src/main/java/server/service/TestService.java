0656c9ad3647ebce692007fb3a5220f692425fd2
hujia <hujia@topjoy.com>
2018-09-13 13:47:11 +0800

    feat: TestNG + PowerMock test

--- src/main/java/server/service/TestService.java
+++ src/main/java/server/service/TestService.java
@@ -1,15 +1,13 @@
 package server.service;
 
+import server.model.TestModel;
+
 /**
  * @author guyue
  * @date 2018/9/13
  */
 public class TestService {
-    public static int add(int a, int b) {
-        return a + value(a);
-    }
-
-    private static int value(int a) {
-        return a;
+    public static int add(int a, TestModel b) {
+        return a + b.getValue();
     }
 }
--- src/test/java/server/service/TestServiceTest.java
+++ src/test/java/server/service/TestServiceTest.java
@@ -1,12 +1,51 @@
 package server.service;
 
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.collections.Lists;
+import server.model.TestModel;
+
+import java.util.Iterator;
+import java.util.List;
 
 @PrepareForTest(TestService.class)
 public class TestServiceTest {
 
-    @Test
-    public void testAdd() {
+    @DataProvider(name = "provider")
+    Iterator<Object[]> provider() {
+        List<Object[]> list = Lists.newLinkedList();
+        list.add(new Object[] { 1, new TestModel(2) });
+        list.add(new Object[] { 3, new TestModel(4) });
+        list.add(new Object[] { 5, new TestModel(6) });
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "provider")
+    public void testAdd(int a, TestModel b) {
+        //setup
+        PowerMockito.spy(TestService.class);
+        TestModel mock = PowerMockito.mock(TestModel.class);
+        try {
+            PowerMockito.when(mock, "getValue").thenReturn(3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //exercise
+        int ret = TestService.add(a, mock);
+        System.out.println("" + a + " + " + b.getValue() + " = " + ret);
+
+        //verify
+        assert ret == a + 3;
+        try {
+            PowerMockito.verifyPrivate(mock, Mockito.times(1)).invoke("getValue");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //teardown
     }
 }
