212d4c1a3c6d3d938b5d75297f048d23b8a9b967
hujia <hujia@topjoy.com>
2018-10-09 21:59:33 +0800

    simple test for jdk proxy

---
+++ src/main/java/client/aop/test/ITest.java
@@ -0,0 +1,13 @@
+package client.aop.test;
+
+/**
+ * @author guyue
+ * @date 2018/10/8
+ */
+public interface ITest {
+    /**
+     * return a integer for test
+     * @return a random integer
+     */
+    public int test();
+}
---
+++ src/main/java/client/aop/test/TestImpl.java
@@ -0,0 +1,12 @@
+package client.aop.test;
+
+/**
+ * @author guyue
+ * @date 2018/10/8
+ */
+public class TestImpl implements ITest {
+    @Override
+    public int test() {
+        return 0;
+    }
+}
